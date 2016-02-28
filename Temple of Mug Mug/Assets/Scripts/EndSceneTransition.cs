1a4a2eb16bd555bcc15657f015f61ef99e4b3081
FrostFyres <frostfyreseo@gmail.com>
2016-02-27 18:39:56 -0600

    Extra Additions Part 1

---
+++ Temple of Mug Mug/Assets/Scripts/EndSceneTransition.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class EndSceneTransition : MonoBehaviour {
+
+	public Image backgroundStart;
+	public Image backgroundEnd;
+
+	public AudioSource stoneDoor;
+	public AudioSource jungleSound;
+
+	public GameObject endCanvas;
+
+	public static bool endSceneStart = false;
+
+	void Start () {
+		
+	}
+	
+	IEnumerator textRead(float waitTime) {
+		yield return new WaitForSeconds(waitTime);
+		endSceneStart = true;
+	}
+
+	void Update () {
+		if(textManager.isMessageDone == true && backgroundEnd.rectTransform.localPosition.y < 2) {
+			StartCoroutine(textRead(3));
+			if(endSceneStart == true) {
+				backgroundStart.rectTransform.position += Vector3.up * 0.5f;
+				backgroundEnd.rectTransform.position += Vector3.up * 0.5f;
+
+					if(!stoneDoor.isPlaying) {
+						stoneDoor.Play();
+					}
+			}
+		}
+		else if(backgroundEnd.rectTransform.localPosition.y >= 2) {
+			stoneDoor.Stop();
+			endCanvas.SetActive(true);
+			if(!jungleSound.isPlaying) {
+				jungleSound.Play();
+			}
+		}
+	}
+}
---
+++ Temple of Mug Mug/Assets/Scripts/KnobManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class KnobManager : MonoBehaviour {
+
+	public Image knobOne;
+	public Image knobTwo;
+	public Image knobThree;
+
+	public static bool isKnobOneCorrect = false;
+	public static bool isKnobTwoCorrect = false;
+	public static bool isKnobThreeCorrect = false;
+
+	private int knobOneCount = 0;
+	private int knobTwoCount = 0;
+	private int knobThreeCount = 0;
+
+	void Start () {
+
+	}
+	
+
+	void Update () {
+		if(knobOneCount == 1) {
+			isKnobOneCorrect = true;
+		}
+		else {
+			isKnobOneCorrect = false;
+		}
+		/*******************************/
+		if(knobTwoCount == 2) {
+			isKnobTwoCorrect = true;
+		}
+
+		else {
+			isKnobTwoCorrect = false;
+		}
+		/********************************/
+		if(knobThreeCount == 3) {
+			isKnobThreeCorrect = true;
+		}
+		else {
+			isKnobThreeCorrect = false;
+		}
+		/********************************/
+	}
+
+	public void rotateKnobOne() {
+		knobOne.transform.Rotate(new Vector3(0,0,-90));
+		knobOneCount++;
+		if(knobOneCount == 4) {
+			knobOneCount = 0;
+		}
+	}
+	public void rotateKnobTwo() {
+		knobTwo.transform.Rotate(new Vector3(0,0,-90));
+		knobTwoCount++;
+		if(knobTwoCount == 4) {
+			knobTwoCount = 0;
+		}
+	}
+	public void rotateKnobThree() {
+		knobThree.transform.Rotate(new Vector3(0,0,-90));
+		knobThreeCount++;
+		if(knobThreeCount == 4) {
+			knobThreeCount = 0;
+		}
+	}
+}
